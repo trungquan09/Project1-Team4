e923269e4b8ed6b64fb14bdeb4995fb9b4335667
hunganh100494 <hunganh100494@gmail.com>
2016-01-09 12:56:52 +0700

    First Class

---
+++ src/main/java/com/ltu12a/team4/project1/Main.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ltu12a.team4.project1;
+
+/**
+ *
+ * @author Prince
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("HELLO BOYS!!!");
+    }
+}
