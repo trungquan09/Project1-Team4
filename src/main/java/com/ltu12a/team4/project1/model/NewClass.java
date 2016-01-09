4d7ace3baf6919dab0f7c8930ad1a509164a3193
TNG <TNG@LP-Lap>
2016-01-09 15:27:22 +0700

    model

---
+++ src/main/java/com/ltu12a/team4/project1/model/NewClass.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.ltu12a.team4.project1.model;
+
+/**
+ *
+ * @author TNG
+ */
+public class NewClass {
+    public static void main(String[] args) {
+        
+    }
+}
